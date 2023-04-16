dc7713e49011c48a97c9e76bd79cb1bedb67af52
forysiak-vova <volodja.forus@gmail.com>
2023-04-16 22:18:02 +0200

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,30 @@
 <template>
-   <navbar></navbar>
-<div class="card">
-   <router-view></router-view>
-</div>
+  <navbar></navbar>
+  <div class="card">
+    <router-view></router-view>
+  </div>
 </template>
 <script>
 import Navbar from "@/Components/Navbar.vue";
 
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
-import Lorem from '@/Components/Lorem.vue'
+// import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/js/bootstrap.js";
 export default {
-   components: {
-      Navbar
-}
-}
+  components: {
+    Navbar,
+  },
+};
 </script>
 <style>
 * {
-   margin: 0;
-   padding: 0;
-   box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
 .card {
-   padding: 0 20px;
-   max-width: 1100px;
-   margin: 50px auto;
+  padding: 0 20px;
+  max-width: 1100px;
+  margin: 50px auto;
 }
 </style>
